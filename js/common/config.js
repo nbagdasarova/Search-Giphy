3fc3f0164f8e7f4c8c60552b45ed1bf5507ecfef
nbagdasarova <110225579+nbagdasarova@users.noreply.github.com>
2022-08-24 18:40:11 +0400

    Update config.js

---
+++ js/common/config.js
@@ -0,0 +1,6 @@
+// search and trends configs
+export const queryUrl='https://api.giphy.com/v1/gifs/search?';
+export const trendUrl='https://api.giphy.com/v1/gifs/trending?';
+export const searchApiKey='aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB';
+export let searchApiQ='query';
+export let searchApiLimit='10';
